79e8f1235602c950286941ce6b0e576fc0e670b6
KARTHIK G SINDUR <61337756+Karthik1969@users.noreply.github.com>
2021-09-03 15:15:30 +0530

    IMPLEMENTATION OF FFT BASED FINGERPRINT REORGANIZATION AND MATCHING SYSTEM
    This is our Academic Project Using Matlab tool, This project has gives more accuracy using less expensive technology ie. FFT.

---
+++ IMPLEMENTATION OF FAST FOURIER TRANSFORM BASED FINGERPRINT REORGANIZATION AND MATCHING SYSTEM/Program Code and Samples/fingerTemplateRead.m
@@ -0,0 +1,7 @@
+function template=fingerTemplateRead
+%dialog for opening fingerprint files
+[templatefile , pathname]= uigetfile('*.dat','Open An Fingerprint template file'); 
+if templatefile ~= 0 
+cd(pathname);
+template=load(char(templatefile));
+end;
